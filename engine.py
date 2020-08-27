6c7194f71e59e02cee566d4bbbf6e9e11df5d848
Kweku <47420306+kwe-k-u@users.noreply.github.com>
2020-08-27 11:49:33 +0000

    refactored into functions

--- engine.py
+++ engine.py
@@ -20,6 +20,11 @@ file = open("WISDM_ar_v1.1/WISDM_ar_v1.1_raw.txt")
 lines = file.readlines()
 
 processedList = []
+balanced_data = pd.DataFrame()
+model = Sequential()
+scaled_x = pd.DataFrame(data = X, columns = ["x","y", "z"])
+
+
 Fs = 20 #frames per second
 #=====================================
 # ploting functions
@@ -74,39 +79,40 @@ def plotThem(activities, data):
 #         print("Error with line", index)
 # =============================================================================
 
-
-for index, fileline in enumerate(lines):
-    #Extracting the data from the file
-    try:
-        line = fileline.split(";")[0].split(",")
-        if line[5] == "":
-            break
-        temp = line
-        processedList.append(temp)
-    except:
-        print("Error with line", index)
+def preprocessing():
+    for index, fileline in enumerate(lines):
+        #Extracting the data from the file
+        try:
+            line = fileline.split(";")[0].split(",")
+            if line[5] == "":
+                break
+            temp = line[0:6]
+            processedList.append(temp)
+        except:
+            print("Error with line", index)
 
 
 #second part
 #balancing data
-columns = ["user", "activity", "time", "x", "y", "z"]
-data = pd.DataFrame(data = processedList, columns = columns)
+def balanceData(balanced_data):
+    columns = ["user", "activity", "time", "x", "y", "z"]
+    data = pd.DataFrame(data = processedList, columns = columns)
 
 
-#Changing the data type of x,y and z values from strings to floats
-data["x"] = data["x"].astype("float")
-data["y"] = data["y"].astype("float")
-data["z"] = data["z"].astype("float")
+    #Changing the data type of x,y and z values from strings to floats
+    data["x"] = data["x"].astype("float")
+    data["y"] = data["y"].astype("float")
+    data["z"] = data["z"].astype("float")
 
-#plotting the data for each activity
-# =============================================================================
-# activities = data["activity"].value_counts().index
-# plotThem(activities, data)
-# =============================================================================
+    #plotting the data for each activity
+    # =============================================================================
+    # activities = data["activity"].value_counts().index
+    # plotThem(activities, data)
+    # =============================================================================
 
 
-#removing timestamp and userid on dataset
-df = data.drop(["user","time"], axis=1).copy()
+    #removing timestamp and userid on dataset
+    df = data.drop(["user","time"], axis=1).copy()
 # =============================================================================
 # print(df.head()) #looking ath the dataframe
 # =============================================================================
@@ -115,24 +121,24 @@ df = data.drop(["user","time"], axis=1).copy()
 # print(data["activity"].value_counts() ) #Gets information about the dataframe sizes
 # =============================================================================
 
-#Balancing the datasets to avoid baises by creating dataframes for each activity
-walking = df[df["activity"] == "Walking"].head(48395).copy()
-jogging = df[df["activity"] == "Jogging"].head(48395).copy()
-upstairs = df[df["activity"] == "Upstairs"].head(48395).copy()
-downstairs = df[df["activity"] == "Downstairs"].head(48395).copy()
-sitting = df[df["activity"] == "Sitting"].head(48395).copy()
-standing = df[df["activity"] == "Standing"].head(48395).copy()
+    #Balancing the datasets to avoid baises by creating dataframes for each activity
+    walking = df[df["activity"] == "Walking"].head(48395).copy()
+    jogging = df[df["activity"] == "Jogging"].head(48395).copy()
+    upstairs = df[df["activity"] == "Upstairs"].head(48395).copy()
+    downstairs = df[df["activity"] == "Downstairs"].head(48395).copy()
+    sitting = df[df["activity"] == "Sitting"].head(48395).copy()
+    standing = df[df["activity"] == "Standing"].head(48395).copy()
 
-#combining
-balanced_data = pd.DataFrame()
-balanced_data = balanced_data.append([walking, jogging, upstairs, downstairs, sitting, standing])
-# =============================================================================
-# print(balanced_data["activity"].value_counts() )
-# =============================================================================
+    #combining
+    balanced_data = balanced_data.append([walking, jogging, upstairs, downstairs, sitting, standing])
+    # =============================================================================
+    # print(balanced_data["activity"].value_counts() )
+    # =============================================================================
 
-#Making the activity a label
-label = LabelEncoder()
-balanced_data["label"] = label.fit_transform(balanced_data["activity"])
+    #Making the activity a label
+    label = LabelEncoder()
+    balanced_data["label"] = label.fit_transform(balanced_data["activity"])
+    return balanced_data
 
 #checking if the encoding worked
 # =============================================================================
@@ -141,45 +147,51 @@ balanced_data["label"] = label.fit_transform(balanced_data["activity"])
 # =============================================================================
 
 #standardizing data
+def standardizeData(scaled_x, balanced_data):
+    X = balanced_data[["x","y","z"]]
+    Y = balanced_data["label"]
 
-X = balanced_data[["x","y","z"]]
-Y = balanced_data["label"]
+    #standardizing x
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
 
-#standardizing x
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
+    scaled_x["label"] = Y.values
 
-scaled_x = pd.DataFrame(data = X, columns = ["x","y", "z"])
-scaled_x["label"] = Y.values
-
-print(scaled_x) #looking at the scaled dataframe
+    print(scaled_x) #looking at the scaled dataframe
+    return (scaled_x)
 
 
 
 
 
 #Frame preparations
-frame_size = Fs * 4 #getting data for 4 seconds (80 samples)
-hop_size = Fs *2 #length of clip
+def prepareFrames(frame_size = Fs *4, hop_size = Fs*2, N_FEATURES = 3):
+# =============================================================================
+#     frame_size = Fs * 4 #getting data for 4 seconds (80 samples)
+#     hop_size = Fs *2 #length of clip
+# =============================================================================
 
-#getting frames
-N_FEATURES = 3
+# =============================================================================
+#     #getting frames
+#         N_FEATURES = 3
+# =============================================================================
 
-frames = []
-labels = []
-for i in range(0, len(scaled_x) - frame_size, hop_size):
-    xTemp = scaled_x["x"].values[i: i + frame_size]
-    yTemp = scaled_x["y"].values[i: i + frame_size]
-    zTemp = scaled_x["z"].values[i: i + frame_size]
+    frames = []
+    labels = []
+    for i in range(0, len(scaled_x) - frame_size, hop_size):
+        xTemp = scaled_x["x"].values[i: i + frame_size]
+        yTemp = scaled_x["y"].values[i: i + frame_size]
+        zTemp = scaled_x["z"].values[i: i + frame_size]
 
-    #retrieving the most used label in segment
-    label = stats.mode(scaled_x["label"][i: i + frame_size])[0][0]
-    frames.append([xTemp, yTemp ,zTemp])
-    labels.append(label)
+        #retrieving the most used label in segment
+        label = stats.mode(scaled_x["label"][i: i + frame_size])[0][0]
+        frames.append([xTemp, yTemp ,zTemp])
+        labels.append(label)
 
 #Bring the segments into a better shape
-frames = np.asarray(frames).reshape(-1, frame_size, N_FEATURES)
-labels = np.asarray(labels)
+    frames = np.asarray(frames).reshape(-1, frame_size, N_FEATURES)
+    labels = np.asarray(labels)
+    return frames, labels
 
 # =============================================================================
 # print("frames")
@@ -195,35 +207,52 @@ labels = np.asarray(labels)
 
 
 #training
-x_train, x_test, y_train, y_test = train_test_split(frames, labels, test_size = 0.2, random_state = 0, stratify = labels)
-print(x_train.shape)
-print(x_test.shape)
+def reshapeData(frames, labels):
+    x_train, x_test, y_train, y_test = train_test_split(frames, labels, test_size = 0.2, random_state = 0, stratify = labels)
+    print(x_train.shape)
+    print(x_test.shape)
 
 
-#reshaping training data into 3d because cnn can't work with 2d data
-x_train = x_train.reshape(5806, 80, 3, 1)
-x_test = x_test.reshape(1452, 80, 3, 1)
+    #reshaping training data into 3d because cnn can't work with 2d data
+    x_train = x_train.reshape(5806, 80, 3, 1)
+    x_test = x_test.reshape(1452, 80, 3, 1)
+    return x_train, x_test, y_train, y_test
 
 
 
 #2d CNN model
-model = Sequential()
-model.add( Conv2D(16, (2,2), activation = "relu", input_shape = x_train[0].shape) )
-model.add(Dropout(0.1))
+def buildModel(model, x_train, x_test, y_train, y_test):
+    model.add( Conv2D(16, (2,2), activation = "relu", input_shape = x_train[0].shape) )
+    model.add(Dropout(0.1))
 
-model.add(Conv2D (32, (2,2), activation = "relu"))
-model.add(Dropout(0.2))
+    model.add(Conv2D (32, (2,2), activation = "relu"))
+    model.add(Dropout(0.2))
 
-model.add(Flatten())
+    model.add(Flatten())
 
-model.add(Dense(64, activation = "relu"))
-model.add(Dropout(0.5))
+    model.add(Dense(64, activation = "relu"))
+    model.add(Dropout(0.5))
 
-model.add(Dense(6, activation = "softmax"))
-
-
-model.compile(optimizer = Adam(learning_rate = 0.001), loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
-history = model.fit(x_train, y_train, epochs = 10, validation_data = (x_test, y_test), verbose =1)
+    model.add(Dense(6, activation = "softmax"))
 
 
-model.save_weights("model weights.h5")
+    model.compile(optimizer = Adam(learning_rate = 0.001), loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
+    history = model.fit(x_train, y_train, epochs = 10, validation_data = (x_test, y_test), verbose =1)
+    return history
+
+def saveModel(model):
+    model.save_weights("model weights.h5")
+
+
+
+
+
+#Running the program
+preprocessing()
+balanced_data = balanceData(balanced_data)
+scaled_x = standardizeData(scaled_x, balanced_data)
+frames, labels = prepareFrames()
+x_train, x_test, y_train, y_test = reshapeData(frames, labels)
+history = buildModel(model, x_train, x_test, y_train, y_test)
+saveModel(model)
+
